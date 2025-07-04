1aff0614ed83a2298f7981af332d44113f4ebd8b
Leonardo López Fimbres <leofimbres1@gmail.com>
2025-07-04 14:36:40 -0700

    Initial commit: Basic OPCO website structure

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
